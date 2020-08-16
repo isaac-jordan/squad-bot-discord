c87617093e4864663fde1f22dacbc7f521d5b171
Isaac Jordan <isaacjordan95@gmail.com>
2020-08-16 23:59:56 +0100

    Respond to !squaddump with a .txt file containing a list of all HLL channels and their members

--- src/discord_helpers.ts
+++ src/discord_helpers.ts
@@ -1,7 +1,43 @@
-import { Message } from "discord.js";
+import {
+  Message, MessageAttachment, CategoryChannel, GuildChannel, GuildChannelManager,
+} from 'discord.js';
+import { convertChannelListToTxt } from './output_helpers';
 
-export const handleMessage = (msg: Message) => {
-    if (msg.content === '!squaddump') {
-        msg.reply('pong');
-    }
-}
+const isCategoryChannel = (channel: GuildChannel): channel is CategoryChannel => { return channel.type === 'category' && (channel as CategoryChannel).children !== undefined; };
+
+const getHellLetLooseVoiceChannels = (channels: GuildChannelManager): GuildChannel[] => {
+  return channels.cache.array()
+    .filter((channel) => { return channel.name.includes('Hell Let Loose'); })
+    .flatMap((channel) => {
+      if (isCategoryChannel(channel)) {
+        return channel.children.array()
+          .filter((childChannel) => { return childChannel.type === 'voice'; });
+      }
+      return [];
+    });
+};
+
+const handleSquadDump = (msg: Message): void => {
+  const { guild } = msg;
+  if (!guild?.available) {
+    console.warn('No guild available for msg.', msg);
+    return;
+  }
+
+  const outputTxt = convertChannelListToTxt(
+    getHellLetLooseVoiceChannels(guild.channels)
+      .sort((a, b) => { return a.rawPosition - b.rawPosition; }),
+  );
+
+  const outputAttachment = Buffer.from(outputTxt, 'utf8');
+
+  const attachment = new MessageAttachment(outputAttachment, 'squads.txt');
+  msg.reply('Here are the current squads in Discord!', attachment)
+    .catch((err) => { return console.error(err); });
+};
+
+export const handleMessage = (msg: Message): void => {
+  if (msg.content === '!squaddump') {
+    handleSquadDump(msg);
+  }
+};
---
+++ src/output_helpers.ts
@@ -0,0 +1,16 @@
+import { GuildChannel } from 'discord.js';
+
+export const convertChannelListToTxt = (channels: GuildChannel[]): string => {
+  const outputArr: string[] = [];
+  channels.forEach((channel) => {
+    console.log(`Members in: ${channel.name}`);
+    outputArr.push(`${channel.name}\n`);
+    channel.members.forEach((member) => {
+      outputArr.push(`${member.user.username}\n`);
+      console.log(member.user.username);
+    });
+    outputArr.push('\n');
+  });
+
+  return outputArr.join('');
+};
